ec6461e8783cc2704fdda8678c3c0c8a78edd781
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-09-23 14:32:07 +0100

    Changed AT1 to test a string including

--- spec/acceptance_tests/1_displays_grid_spec.rb
+++ spec/acceptance_tests/1_displays_grid_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../../lib/tic_tac_toe.rb'
 
 describe TicTacToe do
-  context 'given a new game is started' do
-    context 'when the user requests it' do
+  context 'given the app is started from the command line' do
+    context 'when the user opens the app' do
       # User runs tic_tac_toe.rb from command line
       it 'then display an empty grid' do
         # Empty grid is displayed to the command line
         game = TicTacToe.new
-        expect { game.new_game }.to output(<<~'GRID'
+        expect { game.new_game }.to output(a_string_including(<<~'GRID'
           +----+----+----+
           |    |    |    |
           +----+----+----+
@@ -16,7 +16,7 @@ describe TicTacToe do
           |    |    |    |
           +----+----+----+
         GRID
-                                          ).to_stdout
+                                                             )).to_stdout
       end
     end
   end
