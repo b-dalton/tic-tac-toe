f7def2d97b330a4910cf14ed64f28fa4756077d6
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-09-23 16:04:49 +0100

    Add aruba to project

--- spec/example_test_output_spec.rb
+++ spec/example_test_output_spec.rb
@@ -1,13 +1,32 @@
-class Tester
-  def say_hello
-    puts 'hello world'
-  end
+require 'spec_helper'
+
+RSpec.describe 'First Run', type: :aruba do
+  let(:content) { (<<~'GRID'  # "Act" mixed in with "Assert"
+    +----+----+----+
+    |    |    |    |
+    +----+----+----+
+    |    |    |    |
+    +----+----+----+
+    |    |    |    |
+    +----+----+----+
+  GRID )}
+
+  before(:each) { run_command('ruby ../../lib/tic_tac_toe.rb') }
+
+  # Full string
+  it { expect(last_command_started).to have_output content }
 end
 
-describe Tester do
-  context 'when instructed to say hello' do
-    xit 'prints "hello world" to the command line' do
-      expect { Tester.new.say_hello }.to output('hello world').to_stdout
-    end
-  end
-end
+# class Tester
+#   def say_hello
+#     puts 'hello world'
+#   end
+# end
+
+# describe Tester do
+#   context 'when instructed to say hello' do
+#     xit 'prints "hello world" to the command line' do
+#       expect { Tester.new.say_hello }.to output('hello world').to_stdout
+#     end
+#   end
+# end
