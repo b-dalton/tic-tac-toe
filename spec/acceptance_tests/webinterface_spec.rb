d8e22dfe250970c3ca91ecb3f9693949aaad48c4
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-10-01 12:28:12 +0100

    Test web interface allows user to make a move

--- spec/acceptance_tests/webinterface_spec.rb
+++ spec/acceptance_tests/webinterface_spec.rb
@@ -16,10 +16,29 @@ describe 'tic-tac-toe app' do
 
       # Assert
       expect(last_response).to be_ok
+
       (0..2).each do |row|
         (0..2).each do |col|
           expect(last_response.body).to have_tag('input', with: { class: "row#{row} col#{col}" })
         end
+      end
+    end
+  end
+
+  context 'when a user makes a move' do
+    # Arrange
+    grid_cells_with_css = {
+      row0_col0_in: { cell_css_selector: 'input.row0.col0', input: 'X', expected_output: 'X' }
+    }
+
+    grid_cells_with_css.each do |square, values|
+      it "remembers when user makes a mark in a grid square: '#{square}, #{values[:expected_output]}'" do
+        # Act
+        post '/tictactoe', square => values[:input]
+
+        # Assert
+        expect(last_response).to be_ok
+        expect(last_response.body).to have_tag(values[:square_css_selector], with: { value: values[:expected_output] })
       end
     end
   end
