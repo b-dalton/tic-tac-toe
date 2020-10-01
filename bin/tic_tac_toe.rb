a3376ef19cd90f039fb074127944ee97b0d6f270
Ben <ben.dalton@madetech.com>
2020-10-01 11:28:49 +0100

    /hello page displays in web server

--- bin/tic_tac_toe.rb
+++ bin/tic_tac_toe.rb
@@ -1,14 +1,18 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'erb'
 
 class MyApp < Sinatra::Base
-  get '/' do
+  # enable :sessions
+
+  get '/tictactoe' do
     erb :tictactoe
   end
 
   get '/hello' do
     'Hello world!'
   end
+
+  run! if app_file == $0
 end
 
-# TicTacToe.new.new_game
+
