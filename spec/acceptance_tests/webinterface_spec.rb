be563691aeb9fd38940e5f9a3b882f0f1066c147
Ben <ben.dalton@madetech.com>
2020-10-01 10:13:39 +0100

    wrote first test for sinatra app

---
+++ spec/acceptance_tests/webinterface_spec.rb
@@ -0,0 +1,20 @@
+require '../tic_tac_toe'
+require 'rspec'
+require 'rack/test'
+
+describe 'tic-tac-toe app' do
+    include Rack::Test::Methods
+
+    def app 
+        MyApp
+    end
+
+    context 'when web server is started' do
+        it 'displays a simple welcome message' do
+            get '/'
+            expect(last_response).to be_ok
+            expect(last_response.body).to eq("")
+        end
+        
+    end
+end
